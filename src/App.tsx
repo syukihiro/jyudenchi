823bba32a461bcdb62b114bdc0873bebea7cdcf5
syukihiro <kaui.yukihiro@gmail.com>
2023-09-14 22:46:38 +0900

    up date

--- src/App.tsx
+++ src/App.tsx
@@ -6,61 +6,10 @@ import { ResultPage } from './page';
 function App() {
   const [goalage, setGoalage] = useState<number>();
   const [age, setAge] = useState<number>();
-  const [goalageErr, setGoalageErr] = useState<string>();
-  const [ageErr, setAgeErr] = useState<string>();
   const [openFlg, setOpenflg] = useState<boolean>(false);
-  const disabledFlg = goalage && age && goalageErr == undefined && ageErr == undefined ? false : true;
+  const disabledFlg = goalage && age  ? false : true;
 
-  // TextField 変化時
-  function handleChangeValue (data: string, type: number) {
-    const castData = zenkakuToHankakuNumber(data);
-    const result = Number(castData);
-    const errMsg = "数値を入力してください";
-
-    if(isNaN(result)) {
-      handleValueError(errMsg, type);
-    } else {
-      handleValidValue(result, type);
-    }
-  }
-
-  // エラーメッセージ設定
-  function handleValueError(msg: string, type: number) {
-    if (type === 1) {
-      setGoalageErr(msg);
-    } else if (type === 2) {
-      setAgeErr(msg);
-    }
-  }
-
-  // 値設定
-  function handleValidValue(data: number, type: number) {
-    if (type === 1) {
-      setGoalage(data);
-      setGoalageErr(undefined);
-    } else if (type === 2) {
-      setAge(data);
-      setAgeErr(undefined);
-    }
-  }
-
-  // 半角変換
-  function zenkakuToHankakuNumber(data: string) {
-    const zenkakuNumberMap: Record<string, string> = {
-      '０': '0',
-      '１': '1',
-      '２': '2',
-      '３': '3',
-      '４': '4',
-      '５': '5',
-      '６': '6',
-      '７': '7',
-      '８': '8',
-      '９': '9'
-    };
-    return data.replace(/[０-９]/g, str => zenkakuNumberMap[str] || str);
-  }
-
+  // ボタンクリック時
   function hanbleClickBtn(flg: boolean) {
     setOpenflg(flg);
     if(!flg) {
@@ -78,8 +27,8 @@ function App() {
           justifyContent="center"
           alignItems="center"
           sx={{height: "100%"}}>
-            <TextFileld label='目標とする寿命' type={1} onChange={handleChangeValue} helperText={goalageErr} />
-            <TextFileld label='現在の年齢'    type={2} onChange={handleChangeValue} helperText={ageErr}/>
+            <TextFileld label='目標とする寿命'  onChange={setGoalage} />
+            <TextFileld label='現在の年齢'     onChange={setAge} />
             <Button variant="contained" fullWidth size="large" disabled={disabledFlg} onClick={() => hanbleClickBtn(true)}>START</Button>
         </Stack>
       }
--- src/component/TextField/index.tsx
+++ src/component/TextField/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
 
 interface Props {
   label: string;
-  type: number;
-  helperText?: string;
-  onChange: (data: string, type: number) => void;
+  onChange: (data: number) => void;
 }
 
-export function TextFileld({ label, type, helperText, onChange }:Props) {
+export function TextFileld({ label, onChange }:Props) {
+  const [errMsg, setErrMsg] = useState<string>();
+
+  // TextField 変化時
+  function handleChangeValue (data: string) {
+    const castData = zenkakuToHankakuNumber(data);
+    const result = Number(castData);
+    const errMsg = "数値を入力してください";
+
+    if(isNaN(result)) {
+      setErrMsg(errMsg);
+    } else {
+      onChange(result);
+      setErrMsg(undefined);
+    }
+  }
+
+  // 半角変換
+  function zenkakuToHankakuNumber(data: string) {
+    const zenkakuNumberMap: Record<string, string> = {
+      '０': '0',
+      '１': '1',
+      '２': '2',
+      '３': '3',
+      '４': '4',
+      '５': '5',
+      '６': '6',
+      '７': '7',
+      '８': '8',
+      '９': '9'
+    };
+    return data.replace(/[０-９]/g, str => zenkakuNumberMap[str] || str);
+  }
+
   return (
     <>
       <TextField
@@ -16,13 +47,13 @@ export function TextFileld({ label, type, helperText, onChange }:Props) {
         id="outlined-basic"
         label={label}
         focused={true}
-        color={helperText ? "error" : "primary"}
-        error={helperText ? true : false }
-        helperText={helperText}
+        color={errMsg ? "error" : "primary"}
+        error={errMsg ? true : false }
+        helperText={errMsg}
         margin="normal"
         fullWidth
         variant="outlined"
-        onChange={(e) => onChange && onChange(e.target.value, type)}
+        onChange={(e) => handleChangeValue(e.target.value)}
       />
     </>
   );
